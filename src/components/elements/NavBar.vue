98da765b35aab521b0654d76e17af69ac78ce849
smboolean47 <smboolean47@madrigali.dev>
2022-01-10 10:31:26 +0100

    Add Header

---
+++ src/components/elements/NavBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="nav-bar">
+      <ul class="nav-bar__list">
+        <li class="nav-bar__item">
+            <a href="#">Chi Sono</a>
+        </li>
+        <li class="nav-bar__item">
+            <a href="#">Ultime News</a>
+        </li>
+        <li class="nav-bar__item">
+            <a href="#">Contatti</a>
+        </li>
+      </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+}
+</script>
+
+<style lang="scss" scoped>
+.nav-bar {
+    font-size: 20px;
+    // .nav-bar__list
+    &__list {
+        display: flex;
+        list-style: none;
+        align-items: center;
+        justify-content: flex-end;
+        margin-bottom: 0;
+    }
+    // .nav-bar__item
+    &__item {
+        margin-left: 20px;
+
+        a {
+            color: inherit;
+            text-decoration: none;
+        }
+    }
+}
+</style>
--- src/components/macro/Header.vue
+++ src/components/macro/Header.vue
@@ -1,15 +1,32 @@
 <template>
-    <header>
-
+    <header class="header bg-primary-color">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-4">
+                    <h1>Samuele Madrigali</h1>
+                </div>
+                <div class="col-8">
+                    <NavBar />
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
 <script>
+import NavBar from '../elements/NavBar.vue';
+
 export default {
-    name: 'Header'
+    name: 'Header',
+    components: {
+        NavBar
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.header {
+    padding: 20px 0;
+    border-bottom: 1px solid #fff;
+}
 </style>
